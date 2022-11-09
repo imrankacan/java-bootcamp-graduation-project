85cc43538afc75f6e4104ad792ffc2e7fdd90350
imrankacan <16509032@firat.edu.tr>
2022-11-09 21:12:42 +0300

    beginning part

---
+++ Users.java
@@ -0,0 +1,32 @@
+public class Users {
+    private int id;
+    private String userName;
+    private String userPassword;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsersName() {
+        return userName;
+    }
+
+    public void setUsersName(String usersName) {
+        this.userName = usersName;
+    }
+
+    public String getUsersPassword() {
+        return userPassword;
+    }
+
+    public void setUsersPassword(String usersPassword) {
+        this.userPassword = usersPassword;
+    }
+
+
+
+}
